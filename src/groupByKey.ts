a36a44005b4d73bc555b45bf4e8ffa8dc243c9b1
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-25 21:31:32 +0300

    version 2

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,11 +4,17 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce((groups: GroupsMap<T>, item: T) => {
-    const groupKey = item[key] as string;
+    const groupKey = item[key];
+
+    if (typeof groupKey !== 'string' && typeof groupKey !== 'number') {
+      throw new Error(
+        `Key "${String(key)}" must refer to a string or number property.`,
+      );
+    }
 
     groups[groupKey] = groups[groupKey] || [];
     groups[groupKey].push(item);
 
     return groups;
-  }, {});
+  }, {} as GroupsMap<T>);
 }
